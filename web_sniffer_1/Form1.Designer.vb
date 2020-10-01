89d20671aac8e020271a4dc5db619a749c50555a
Marc <marc.palomar.soler@gmail.com>
2020-10-01 23:47:57 +0200

    Initial commit

---
+++ web_sniffer_1/Form1.Designer.vb
@@ -0,0 +1,348 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.urlBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.runningTimelbl = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.wordsToFindtxt = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'urlBox1
+        '
+        Me.urlBox1.Location = New System.Drawing.Point(36, 49)
+        Me.urlBox1.Name = "urlBox1"
+        Me.urlBox1.Size = New System.Drawing.Size(572, 20)
+        Me.urlBox1.TabIndex = 0
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Web url to sniff"
+        '
+        'runningTimelbl
+        '
+        Me.runningTimelbl.Location = New System.Drawing.Point(36, 471)
+        Me.runningTimelbl.Name = "runningTimelbl"
+        Me.runningTimelbl.ReadOnly = True
+        Me.runningTimelbl.Size = New System.Drawing.Size(100, 20)
+        Me.runningTimelbl.TabIndex = 2
+        Me.runningTimelbl.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button1
+        '
+        Me.Button1.AccessibleDescription = ""
+        Me.Button1.AccessibleName = ""
+        Me.Button1.Location = New System.Drawing.Point(241, 471)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(102, 20)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Test button"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'wordsToFindtxt
+        '
+        Me.wordsToFindtxt.Location = New System.Drawing.Point(36, 121)
+        Me.wordsToFindtxt.Multiline = True
+        Me.wordsToFindtxt.Name = "wordsToFindtxt"
+        Me.wordsToFindtxt.Size = New System.Drawing.Size(332, 193)
+        Me.wordsToFindtxt.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 4)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(16, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "..."
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(610, 444)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(133, 47)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Start"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(142, 471)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(82, 20)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Location = New System.Drawing.Point(392, 121)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(351, 193)
+        Me.Panel1.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(33, 452)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Time sniffing"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(139, 452)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(91, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Interval (seconds)"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(641, 49)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(102, 23)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Go to web"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(156, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Key Words (separated by enter)"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(389, 101)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(37, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Result"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(105, 52)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(181, 20)
+        Me.TextBox2.TabIndex = 15
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 12)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(77, 17)
+        Me.CheckBox1.TabIndex = 16
+        Me.CheckBox1.Text = "Email alert "
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(468, 52)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(220, 20)
+        Me.TextBox3.TabIndex = 17
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(465, 33)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(73, 13)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "Asunto correu"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Button6)
+        Me.Panel2.Controls.Add(Me.Button5)
+        Me.Panel2.Controls.Add(Me.Button4)
+        Me.Panel2.Controls.Add(Me.Label9)
+        Me.Panel2.Controls.Add(Me.Label8)
+        Me.Panel2.Controls.Add(Me.TextBox4)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.TextBox2)
+        Me.Panel2.Controls.Add(Me.TextBox3)
+        Me.Panel2.Controls.Add(Me.CheckBox1)
+        Me.Panel2.Location = New System.Drawing.Point(36, 328)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(707, 89)
+        Me.Panel2.TabIndex = 19
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(626, 9)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(62, 23)
+        Me.Button6.TabIndex = 22
+        Me.Button6.Text = "Load"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(554, 9)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(62, 23)
+        Me.Button5.TabIndex = 20
+        Me.Button5.Text = "Save"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(11, 53)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(77, 20)
+        Me.Button4.TabIndex = 20
+        Me.Button4.Text = "Permission"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(294, 33)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(81, 13)
+        Me.Label9.TabIndex = 21
+        Me.Label9.Text = "Gmail password"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(102, 33)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(87, 13)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "Email (only gmail)"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(297, 52)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(150, 20)
+        Me.TextBox4.TabIndex = 20
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(794, 508)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.wordsToFindtxt)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.runningTimelbl)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.urlBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Web Sniffer"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents urlBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents runningTimelbl As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents wordsToFindtxt As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+End Class
---
+++ web_sniffer_1/Form1.vb
@@ -0,0 +1,316 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Mail
+
+Public Class Form1
+
+    Public dataDirectory As String = My.Application.Info.DirectoryPath
+
+    Public timeSniffing As Integer
+    Public sniffInterval As Integer
+    Public sniffIntervalMinimum As Integer = 3
+    Public LastSniffed As Integer
+
+    Public sniffing As Boolean = False
+    Public objectsFound As String = ""
+
+    Public newLine As String = vbCrLf
+
+    Private passwordGmailSave As String
+
+    Public webClient As System.Net.WebClient
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'urlBox1.Text = "https://www.naturaselection.com/es/tienda/ropa/coleccion-organica"
+        'urlBox1.Text = "https://www.nike.com/es/ca/home"
+        'urlBox1.Text = "https://es.louisvuitton.com/esp-es/hombre/carteras-y-pequena-marroquineria/todas-las-carteras-y-pequena-marroquineria/_/N-5ts7t2"
+
+        LastSniffed = 0
+        runningTimelbl.Text = "00:00:00"
+        Button2.Text = "Start"
+        CheckBox1.Checked = False
+        StopSniffing()
+
+    End Sub
+    'Use in case you want to keep the same connexion for every interval, not working tho
+    Private Sub SetWebConnexion()
+        webClient = New System.Net.WebClient()
+        webClient.UseDefaultCredentials = True
+        webClient.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials
+        webClient.Headers.Set("User-Agent", "Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>")
+        'webClient.Headers.Set(HttpRequestHeader.KeepAlive, "300")
+        'webClient.Headers.Set(HttpRequestHeader.Connection, "keep-alive")
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        If (TextBox1.Text = "") Then
+            TextBox1.Text = sniffIntervalMinimum.ToString
+        End If
+
+        'Sets a minimuminterval time for the system can complete the html search between sniffs
+        sniffInterval = Math.Max(Convert.ToInt32(TextBox1.Text), sniffIntervalMinimum)
+        TextBox1.Text = sniffInterval.ToString
+
+        If sniffing Then
+            timeSniffing += 1
+            runningTimelbl.Text = SecondsToTime(timeSniffing)
+
+            If timeSniffing - LastSniffed >= sniffInterval Then
+                Sniff()
+                LastSniffed = timeSniffing
+
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub Sniff()
+        AnalyzeHTML()
+    End Sub
+
+    Private Sub GetHTML()
+        Dim request As WebRequest = WebRequest.Create(urlBox1.Text)
+        request.UseDefaultCredentials = True
+        request.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials
+
+        Using response As WebResponse = request.GetResponse()
+            Using reader As New StreamReader(response.GetResponseStream())
+                Dim html As String = reader.ReadToEnd()
+                File.WriteAllText(dataDirectory + "\page.html", html)
+            End Using
+        End Using
+    End Sub
+
+
+    Private Sub AnalyzeHTML()
+        If (urlBox1.Text = "") Then
+            StopSniffing()
+            MsgBox("Web url empty. Sniffing will not start")
+
+            Return
+        End If
+
+        webClient = New System.Net.WebClient()
+        webClient.UseDefaultCredentials = True
+        webClient.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials
+        webClient.Headers.Set("User-Agent", "Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>")
+
+        'sense aquest header LV no m'acceptava
+        webClient.Headers.Add("Accept", "text / html, application / xhtml + xml, image / jxr, */*")
+
+        webClient.Headers.Add("Accept-Language", "pt - PT, pt; q = 0.8, en - US; q = 0.5, en; q = 0.3")
+        webClient.Headers.Add("Cookie", "_ga = GA1.3.1173403303.1470761279; tw =% 7B % 22browse % 22 % 3A1134 % 7D; userinfo = __01658d30ffdd0b479894 % 3B % 7B % 22username % 22 % 3A % 22 % 22 % 2C % 22uniqueid % 22 % 3A % 22a5dc80fe2a7aa790049fced4be5d0f39 % 22 % 2C % 22vd % 22 % 3A % 22BXqgk9 % 2CBYJFwd % 2CA % 2CS % 2CA % 2C % 2CL % 2CB % 2CB % 2CBYJJOx % 2CBYJJhI % 2CO % 2CD % 2CJ % 2CBYJJhI % 2C13 % 2CA % 2CB % 2CA % 2C % 2CB % 2CA % 2CB % 2CA % 2CA % 2C % 2CA % 22 % 2C % 22attr % 22 % 3A67108880 % 7D; _ga = GA1.2.1173403303.1470761279; __qca = P0 - 724951276 - 1470761281462; __gads = ID = f00ea51d533ffef6:T = 1472910412:S = ALNI_MZThZH7YlaHm_VKaQ_TH2Hik7nsmQ; _gat = 1")
+        webClient.Headers.Add("DNT", "1")
+
+        Dim sourceString As String = ""
+
+        Try
+            sourceString = webClient.DownloadString(urlBox1.Text)
+        Catch ex As Exception
+            StopSniffing()
+            MsgBox(ex)
+        End Try
+
+
+        Dim keyWords() As String = wordsToFindtxt.Text.Split(New String() {Environment.NewLine},
+                                       StringSplitOptions.None)
+        Dim resultText As String = ""
+
+        Dim haTrobat As Boolean = False
+        objectsFound = ""
+
+        For Each keyWord In keyWords
+            If keyWord <> "" Then
+                If sourceString.ToLower.Contains(keyWord.ToLower) Then
+                    resultText += "Trobat: " + keyWord + vbCrLf
+
+                    objectsFound += keyWord + ","
+                    haTrobat = True
+                Else
+                    resultText += "No trobat: " + keyWord + vbCrLf
+                End If
+            End If
+        Next
+
+
+        resultText += "Sniffed at: " + Now
+        Label3.Text = resultText
+
+        If (haTrobat And CheckBox1.Checked) Then
+            SendEmail()
+            StopSniffing()
+        End If
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Sniff()
+        GetHTML()
+    End Sub
+
+    Private Function SecondsToTime(ByVal seconds) As String
+
+        Dim mHours As Long, mMinutes As Long, mSeconds As Long
+        mSeconds = seconds
+        mHours = mSeconds \ 3600
+        mMinutes = (mSeconds - (mHours * 3600)) \ 60
+        mSeconds = mSeconds - ((mHours * 3600) + (mMinutes * 60))
+
+        Dim sSeconds As String = mSeconds
+        Dim sMinutes As String = mMinutes
+        Dim sHours As String = mHours
+
+        If mSeconds < 10 Then sSeconds = "0" + sSeconds
+        If mMinutes < 10 Then sMinutes = "0" + sMinutes
+        If mHours < 10 Then sHours = "0" + sHours
+
+        Return sHours & ":" & sMinutes & ":" & sSeconds
+
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (sniffing) Then
+            StopSniffing()
+
+        Else
+            StartSniffing()
+
+        End If
+    End Sub
+
+    Public Sub StartSniffing()
+        If (CheckEmailEmpty()) Then
+            StopSniffing()
+            Return
+        End If
+
+        'SetWebConnexion()
+
+        Panel2.Enabled = False
+        sniffing = True
+        Button2.Text = "Stop"
+
+        timeSniffing = 0
+        LastSniffed = -(sniffInterval - 1)
+
+        wordsToFindtxt.ReadOnly = True
+        TextBox1.ReadOnly = True
+        urlBox1.ReadOnly = True
+
+        If TextBox4.Text <> "************" And TextBox4.Text <> "" Then
+            passwordGmailSave = TextBox4.Text
+            TextBox4.Text = "************"
+        End If
+
+
+    End Sub
+    Public Sub StopSniffing()
+        Panel2.Enabled = True
+        sniffing = False
+        Button2.Text = "Start"
+
+        wordsToFindtxt.ReadOnly = False
+        urlBox1.ReadOnly = False
+        TextBox1.ReadOnly = False
+
+    End Sub
+
+    Public Function CheckEmailEmpty() As Boolean
+
+        If CheckBox1.Checked And (TextBox2.Text = "" Or TextBox2.Text = "" Or TextBox2.Text = "") Then
+            MsgBox("Email alert activated but email parameters not completed. Sniff will not start")
+            Return True
+        End If
+
+        Return False
+
+    End Function
+
+    Public Sub SendEmail()
+
+        Try
+            Dim fromAux As String = TextBox2.Text
+            Dim netEmail As New System.Net.Mail.MailMessage
+
+            Dim smtpEmail As New System.Net.Mail.SmtpClient
+
+            smtpEmail.Host = "smtp.gmail.com"
+            smtpEmail.Port = CInt(587)
+            smtpEmail.DeliveryMethod = Net.Mail.SmtpDeliveryMethod.Network
+            'smtpEmail.UseDefaultCredentials = bAutenticacionServidorSMTP
+            smtpEmail.Credentials = New System.Net.NetworkCredential(fromAux.Trim, passwordGmailSave)
+            smtpEmail.EnableSsl = True
+            netEmail.IsBodyHtml = True
+            netEmail.Priority = Net.Mail.MailPriority.Normal
+            netEmail.From = New System.Net.Mail.MailAddress(fromAux)
+
+            Dim assumpteCorreu As String = TextBox3.Text
+            If assumpteCorreu = "" Then
+                assumpteCorreu = "Tierra a la vista!"
+            End If
+
+            Dim cosCorreu As String = "S'ha trobat les següents keywords en la pàgina: " + urlBox1.Text + newLine + "Hora: " + Now.ToString
+            Dim keywordsAux As String() = objectsFound.Split(",")
+            For Each keyword In keywordsAux
+                If keyword <> "" Then
+                    cosCorreu += newLine + "-> " + keyword
+                End If
+            Next
+
+            netEmail.Subject = assumpteCorreu
+
+            Dim htmlView As Net.Mail.AlternateView = Net.Mail.AlternateView.CreateAlternateViewFromString(cosCorreu)
+            netEmail.AlternateViews.Add(htmlView)
+
+            netEmail.To.Clear()
+            netEmail.To.Add(fromAux)
+
+
+            smtpEmail.Send(netEmail)
+        Catch ex As Exception
+            StopSniffing()
+            MsgBox("EMAIL ERROR:" + ex.ToString + newLine + "IMPORTANT: Es possible que falti activar els permisos de gmail, apretar botó 'Permission'")
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'boto de goto web
+        Process.Start(urlBox1.Text)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'link de google que s'ha d'acceptar per poder rebre emails
+        Process.Start("https://myaccount.google.com/u/0/lesssecureapps?pli=1")
+    End Sub
+
+    Private Sub saveData()
+        SaveSetting(My.Application.Info.ProductName, "Settings", "email", TextBox2.Text)
+        If TextBox4.Text = "************" Then
+            SaveSetting(My.Application.Info.ProductName, "Settings", "password", passwordGmailSave)
+        Else
+            SaveSetting(My.Application.Info.ProductName, "Settings", "password", TextBox4.Text)
+        End If
+        SaveSetting(My.Application.Info.ProductName, "Settings", "assumpte", TextBox3.Text)
+    End Sub
+
+    Private Sub loadData()
+        TextBox2.Text = GetSetting(My.Application.Info.ProductName, "Settings", "email")
+        passwordGmailSave = GetSetting(My.Application.Info.ProductName, "Settings", "password")
+        TextBox4.Text = "************"
+        TextBox3.Text = GetSetting(My.Application.Info.ProductName, "Settings", "assumpte")
+        CheckBox1.Checked = True
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        saveData()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        loadData()
+    End Sub
+End Class
